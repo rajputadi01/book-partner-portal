92cc4d3e8975bf97e1887b04dcaef5c44de544d5
Aditya Rajput <rajputaditya.0203@gmail.com>
2026-04-09 12:02:33 +0530

    Initial commit: Project setup, pom.xml dependencies, and database config

---
+++ src/main/java/com/capg/portal/BookPartnerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.capg.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPartnerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPartnerPortalApplication.class, args);
+	}
+
+}
