fa3f61de66e135e6bd808bf60058f5c41b189f17
Aditya Rajput <rajputaditya.0203@gmail.com>
2026-04-09 14:10:29 +0530

    Created titles and employee entities with validation

--- src/main/java/com/capg/portal/catalog/entity/Title.java
+++ src/main/java/com/capg/portal/catalog/entity/Title.java
@@ -8,7 +8,43 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "titles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Title {
 
+    @Id
+    @Column(name = "title_id", length = 6)
+    private String titleId;
 
+    @Column(name = "title", nullable = false, length = 80)
+    private String titleName; // 'titleName' to avoid clashing with class name
+
+    @Column(name = "type", columnDefinition = "char(12)")
+    private String type;
+
+    // Foreign Key mapping to Publishers table
+    @ManyToOne
+    @JoinColumn(name = "pub_id")
+    private Publisher publisher;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "advance")
+    private Double advance;
+
+    @Column(name = "royalty")
+    private Integer royalty;
+
+    @Column(name = "ytd_sales")
+    private Integer ytdSales;
+
+    @Column(name = "notes", length = 200)
+    private String notes;
+
+    @Column(name = "pubdate")
+    private LocalDateTime pubdate;
 }
